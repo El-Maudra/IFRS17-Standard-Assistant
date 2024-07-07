a09a484185d8da35129504c30d0d3a4340aceae5
Walter Nathaniel <wltr.nathaniel@gmail.com>
2024-07-07 23:48:44 +0300

    Implement the Open AI environments and the application

---
+++ app.py
@@ -0,0 +1,91 @@
+import os
+from dotenv import load_dotenv
+import openai, requests, json, time, logging
+import streamlit as st
+from datetime import datetime
+
+# Load environment variables
+load_dotenv()
+
+client = openai.OpenAI()
+
+model = "gpt-4-1106-preview"
+
+# Step 1: Upload a file to Open AI embeddings
+filepath = "./IFRS17.pdf"
+file_object = client.files.create(file=open(filepath, "rb"),
+                                  purpose="assistants")
+
+# Step 2: Create an assistant
+assistant = client.beta.assistants.create(
+    name="IFRS 17 Assistant",
+    instructions = """You are a helpful study assistant who knows a lot about understanding IFRS 17 Standard by IASB. Your role is to summarize standard, clarify terminology within context, and extract key information and formulas. Cross-reference information for additional insights and answer related questions comprehensively. Analyze the standard, key changes and where applicable. Respond to queries effectively, incorporating feedback to enhance your accuracy. Handle data securely and update your knowledge base with the latest standards and any similar research on the same. Adhere to ethical standards, respect intellectual property, and provide users with guidance on the standard. Maintain a feedback loop for continuous improvement and user support. Your ultimate goal is to facilitate a deeper understanding of complex IFRS 17 Standard material, making it more accessible and comprehensible.""",
+    tool = [{type: "retrieval"}],
+    model=model,
+    file_ids=[file_object.id]
+)
+
+# Get the Assistant ID
+asiss_id = assistant.id
+print(asiss_id)
+
+# Hardcoded ids to be used once the first code run is done and the assistant was created
+thread_id = None
+assiss_id = None
+
+# == Step 3. Create a Thread
+message = "What is mining?"
+
+thread = client.beta.threads.create()
+thread_id = thread.id
+print(thread_id)
+
+# message = client.beta.threads.messages.create(
+#     thread_id=thread_id, role="user", content=message
+# )
+
+# == Run the Assistant
+run = client.beta.threads.runs.create(
+    thread_id=thread_id,
+    assistant_id=assis_id,
+    instructions="Please address the user as Bruce",
+)
+
+def wait_for_run_completion(client, thread_id, run_id, sleep_interval=5):
+    """
+    Waits for a run to complete and prints the elapsed time.:param client: The OpenAI client object.
+    :param thread_id: The ID of the thread.
+    :param run_id: The ID of the run.
+    :param sleep_interval: Time in seconds to wait between checks.
+    """
+    while True:
+        try:
+            run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
+            if run.completed_at:
+                elapsed_time = run.completed_at - run.created_at
+                formatted_elapsed_time = time.strftime(
+                    "%H:%M:%S", time.gmtime(elapsed_time)
+                )
+                print(f"Run completed in {formatted_elapsed_time}")
+                logging.info(f"Run completed in {formatted_elapsed_time}")
+                # Get messages here once Run is completed!
+                messages = client.beta.threads.messages.list(thread_id=thread_id)
+                last_message = messages.data[0]
+                response = last_message.content[0].text.value
+                print(f"Assistant Response: {response}")
+                break
+        except Exception as e:
+            logging.error(f"An error occurred while retrieving the run: {e}")
+            break
+        logging.info("Waiting for run to complete...")
+        time.sleep(sleep_interval)
+
+
+# Run it
+wait_for_run_completion(client=client, 
+                        thread_id=thread.id, 
+                        run_id=run.id)
+
+# Check the run steps LOGS
+run_steps = client.beta.threads.runs.steps.list(thread_id=thread_id, run_id=run.id)
+print(f"Run Steps --> {run_steps.data[0]}")
